5af12d93edb7c0a89b11470b21187a9b728a656f
cyrilscript <lovelyjhames@gmail.com>
2023-09-09 21:12:57 +0100

    updates

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import DeviceInfo from "react-native-device-info";
+import "expo-dev-client";
+import { useState } from "react";
+import { Feather, Entypo } from "@expo/vector-icons";
 
 export default function App() {
+  const [deviceID, setdeviceID] = useState("");
+  const getDeviceUniqueID = async () => {
+    const res = await DeviceInfo.getUniqueId();
+    setdeviceID(res);
+    console.log(res);
+  };
+  getDeviceUniqueID();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Feather name="edit-2" size={14} color="black" />
+
+      <Text>Unique ID: {deviceID}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
